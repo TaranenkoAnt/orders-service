c91354a0b7ca61f08cee2596cd914a28dcc35eb3
taranenko <taranenko@reshenie-soft.ru>
2026-02-05 10:22:59 +0300

    refs #8

--- src/test/java/ru/javaops/cloudjava/ordersservice/controller/MenuOrderControllerTest.java
+++ src/test/java/ru/javaops/cloudjava/ordersservice/controller/MenuOrderControllerTest.java
@@ -7,13 +7,16 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import ru.javaops.cloudjava.ordersservice.BaseIntegrationTest;
 import ru.javaops.cloudjava.ordersservice.dto.OrderResponse;
+import ru.javaops.cloudjava.ordersservice.storage.model.MenuOrder;
 import ru.javaops.cloudjava.ordersservice.storage.model.OrderStatus;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 import static ru.javaops.cloudjava.ordersservice.controller.MenuOrderController.USER_HEADER;
 import static ru.javaops.cloudjava.ordersservice.testdata.TestConstants.*;
-import static ru.javaops.cloudjava.ordersservice.testdata.TestDataProvider.createOrderRequest;
-import static ru.javaops.cloudjava.ordersservice.testdata.TestDataProvider.createdItems;
+import static ru.javaops.cloudjava.ordersservice.testdata.TestDataProvider.*;
 
 @AutoConfigureWebTestClient(timeout = "20000")
 class MenuOrderControllerTest extends BaseIntegrationTest {
@@ -54,5 +57,55 @@ class MenuOrderControllerTest extends BaseIntegrationTest {
                 .bodyValue(request)
                 .exchange()
                 .expectStatus().isNotFound();
+    }
+
+    @Test
+    void getOrderList_returnsCorrectSortedOrders() {
+        prepareStubForSuccess();
+        List<MenuOrder> orders = new ArrayList<>();
+
+        webTestClient.get()
+                .uri(BASE_URL + "?sort={sort}&from={from}&size={size}")
+                .header(USER_HEADER, USERNAME_ONE)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody();
+    }
+
+    @Test
+    void submitMenuOrder_returnsBadRequest_whenItIncorrect() {
+        var request = createOrderInvalidRequest();
+        webTestClient.post()
+                .uri(BASE_URL)
+                .header(USER_HEADER, USERNAME_ONE)
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(request)
+                .exchange()
+                .expectStatus().isBadRequest();
+    }
+
+    @Test
+    void getOrderList_returnsServiceUnavailable() {
+        prepareStubForSuccessWithTimeout();
+        List<MenuOrder> orders = new ArrayList<>();
+
+        webTestClient.get()
+                .uri(BASE_URL + "?sort={sort}&from={from}&size={size}")
+                .header(USER_HEADER, USERNAME_ONE)
+                .exchange()
+                .expectStatus().isBadRequest()
+                .expectBody();
+    }
+
+    @Test
+    void getOrderList_returnsSBadRequest_whenHaveValidationError() {
+        prepareStubForSuccess();
+
+        webTestClient.get()
+                .uri(BASE_URL + "?sort={sort}&from={from}&size={size}")
+                .header(USER_HEADER, USERNAME_ONE)
+                .exchange()
+                .expectStatus().isBadRequest()
+                .expectBody();
     }
 }
